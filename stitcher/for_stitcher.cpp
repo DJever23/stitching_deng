5fe46055153c063a2864ff1fb031151feef4e211
DJever23 <13262218151@163.com>
2020-01-07 15:35:33 +0800

    裁剪照片

--- stitcher/for_stitcher.cpp
+++ stitcher/for_stitcher.cpp
@@ -5,6 +5,12 @@
 #include <opencv2/stitching.hpp>
 //#include "windows.h"
 #include<time.h>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+
 
 using namespace std;
 using namespace cv;
@@ -12,7 +18,229 @@ using namespace cv;
 //bool try_use_gpu = false;
 vector<Mat> imgs;
 string result_name = "dst1.jpg";
-int main()
+/*尝试修改成c++代码失败
+Mat gray,ret,thresh,cnts,hierarchy;
+status = cv::copyMakeBorder(status, 10, 10, 10, 10, cv::BORDER_CONSTANT, (0, 0, 0));
+cv::cvtColor(status, gray, CV_BGR2GRAY);
+ret, thresh = cv::threshold(gray, 0, 255, cv::THRESH_BINARY);
+
+cnts, hierarchy = cv::findContours(thresh.copy(), cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+cnt = max(cnts, key=cv::contourArea);
+
+//mask = np.zeros(thresh.shape, dtype="uint8")
+vector<Point2f> mask;
+x, y, w, h = cv::boundingRect(cnt);
+cv::rectangle(mask, (x, y), (x + w, y + h), 255, -1);
+
+minRect = mask.clone();
+sub = mask.copy();
+
+// 开始while循环，直到sub中不再有前景像素
+while cv::countNonZero(sub) > 0
+    {
+    minRect = cv2.erode(minRect, None);
+    sub = cv2.subtract(minRect, thresh);
+    }
+cnts, hierarchy = cv::findContours(minRect.copy(), cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+cnt = max(cnts, key=cv::contourArea);
+x, y, w, h = cv::boundingRect(cnt);
+
+//使用边界框坐标提取最终的全景图
+stitched = stitched[y:y + h, x:x + w];
+
+cv::imwrite("final.jpg", stitched);
+*/
+    //cout << "Time: "<<t1 - t0 << endl;
+
+/*使用python去除拼接后的黑边，参考知乎：https://zhuanlan.zhihu.com/p/83225676
+stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (0, 0, 0))
+gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+
+cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnt = max(cnts, key=cv2.contourArea)
+
+mask = np.zeros(thresh.shape, dtype="uint8")
+x, y, w, h = cv2.boundingRect(cnt)
+cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
+
+minRect = mask.copy()
+sub = mask.copy()
+
+# 开始while循环，直到sub中不再有前景像素
+while cv2.countNonZero(sub) > 0:
+    minRect = cv2.erode(minRect, None)
+    sub = cv2.subtract(minRect, thresh)
+
+cnts, hierarchy = cv2.findContours(minRect.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnt = max(cnts, key=cv2.contourArea)
+x, y, w, h = cv2.boundingRect(cnt)
+
+# 使用边界框坐标提取最终的全景图
+stitched = stitched[y:y + h, x:x + w]
+
+cv2.imwrite('final.jpg', stitched)
+*/
+
+/*************************************************
+ * author : wyj
+ * date : 2016-10-31
+ * **********************************************/ 
+
+const int black = 10;
+
+
+/**************************************************
+ * remove the black border
+ * ***********************************************/ 
+class imageRemove{
+public:
+	imageRemove(string imageName,string saveImageName);
+	string getRawImageName();
+	void setSaveImageName(string a);
+	int removeblack();
+private:
+	string saveImageName;
+	string imageName;
+};
+
+imageRemove::imageRemove(string imageName, string saveImageName) 
+{	
+	this->imageName = imageName;
+	this->saveImageName = saveImageName;
+}
+
+string imageRemove::getRawImageName()
+{
+	return imageName;
+}
+
+void imageRemove::setSaveImageName(string a)
+{	
+	this->saveImageName = a;
+}
+
+// remove black border
+
+int imageRemove::removeblack()
+{
+	Mat inputImg = imread(imageName);
+	if(!inputImg.data)
+	{
+		cerr<<"read image error:\t"<<imageName;
+		return 1;
+	}
+	if(!inputImg.isContinuous())
+	{
+		return 2;
+	}
+	Mat tmpMat = inputImg.clone();
+	cvtColor(tmpMat, tmpMat, CV_RGB2GRAY);
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < 5; j++)
+		{
+			cout<<(int)tmpMat.at<uchar>(i,j)<<" ";
+		}
+		cout<<endl;
+	}
+
+	int left = 0;
+	int right = tmpMat.cols;
+	int up = 0;
+	int down = tmpMat.rows;
+	// 
+	int num = 0;
+	//int range = tmpMat.cols / 5;
+	for(int i = 0; i < tmpMat.rows; i++)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.cols; j++)
+		{
+			if((int)tmpMat.at<uchar>(i,j) >= black)
+			{
+				//cout<<"(i,j)="<<tmpMat<<endl;
+				num++;
+			}
+		}
+		if(num >=0.65* tmpMat.cols)
+		{
+			up = i;
+			break;
+		}
+	}
+	for(int i = tmpMat.rows-1; i >= 0; i--)
+	{
+		num  = 0;
+		for(int j = 0; j < tmpMat.cols; j++)
+		{
+			if((int)tmpMat.at<uchar>(i,j) >= black)
+			{
+				num ++;
+			}
+		}
+		if(num >=0.85* tmpMat.cols)
+		{
+			down = i;
+			break;
+		}
+	}
+	//range = tmpMat.rows / 5;
+	for(int i = 0; i < tmpMat.cols; i++)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.rows; j++)
+		{
+			if((int)tmpMat.at<uchar>(j,i) >= black)
+			{
+				num++;
+			}
+		}
+		if(num >=0.8* tmpMat.rows)
+		{
+			left = i;
+			break;
+		}
+	}
+	for(int i = tmpMat.cols-1; i >= 0; i--)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.rows; j++)
+		{
+			if((int)tmpMat.at<uchar>(j,i) >= black)
+			{
+				num++;
+			}
+		}
+		if(num >=0.8* tmpMat.rows)
+		{
+			right = i;
+			break;
+		}
+	}
+	cout<<up<<" "<<down<<" "<<left<<" "<<right<<endl;
+	if(up > down || right < left)
+	{
+		cerr<<"image is total black:\t"<<imageName;
+		return 3;
+	}
+	Rect rect(left, up, right - left, down - up);
+	Mat imageSave = inputImg(rect);
+	imshow(saveImageName,imageSave);
+	imwrite(saveImageName, imageSave);
+	waitKey();
+	return 0;
+}
+
+// 
+
+void usage()
+{
+	cout<<"*******************************************"<<endl;
+	cout<<"./removeblack inputImage outPutImage"<<endl;
+}
+
+int main(int argc, char** argv)
 {
 	//Mat  stitchedImage;
 	int n;
@@ -61,9 +289,18 @@ int main()
     // 显示源图像，和结果图像
     imshow("全景图像", pano);
 
-    //cout << "Time: "<<t1 - t0 << endl;
-
     waitKey();
+    /*if(argc != 3)
+	{
+		cerr<<"parameter error";
+		usage();
+		return 1;
+	}*/
+	string inputFileName = (result_name);
+	string outputFileName = ("out_pic.jpg");
+	imageRemove  iR(inputFileName, outputFileName);
+	iR.removeblack();
+
     return 0;
 }
 
--- stitcher/source_stitcher.cpp
+++ stitcher/source_stitcher.cpp
@@ -2,6 +2,12 @@
 #include "opencv2/highgui/highgui.hpp"  
 #include "opencv2/stitching.hpp"  
 #include <iostream>  
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <opencv2/imgproc/types_c.h>
+
+
  
 using namespace cv;
 using namespace std;
@@ -10,6 +16,166 @@ vector<Mat> imgs; //保存拼接的原始图像向量
 //导入所有原始拼接图像函数  
 void parseCmdArgs(int argc, char** argv);
  
+
+ 
+//导入所有原始拼接图像函数  
+/*************************************************
+ * author : wyj
+ * date : 2016-10-31
+ * **********************************************/ 
+
+const int black = 10;
+
+
+/**************************************************
+ * remove the black border
+ * ***********************************************/ 
+class imageRemove{
+public:
+	imageRemove(string imageName,string saveImageName);
+	string getRawImageName();
+	void setSaveImageName(string a);
+	int removeblack();
+private:
+	string saveImageName;
+	string imageName;
+};
+
+imageRemove::imageRemove(string imageName, string saveImageName) 
+{	
+	this->imageName = imageName;
+	this->saveImageName = saveImageName;
+}
+
+string imageRemove::getRawImageName()
+{
+	return imageName;
+}
+
+void imageRemove::setSaveImageName(string a)
+{	
+	this->saveImageName = a;
+}
+
+// remove black border
+
+int imageRemove::removeblack()
+{
+	Mat inputImg = imread(imageName);
+	if(!inputImg.data)
+	{
+		cerr<<"read image error:\t"<<imageName;
+		return 1;
+	}
+	if(!inputImg.isContinuous())
+	{
+		return 2;
+	}
+	Mat tmpMat = inputImg.clone();
+	cvtColor(tmpMat, tmpMat, CV_RGB2GRAY);
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < 5; j++)
+		{
+			cout<<(int)tmpMat.at<uchar>(i,j)<<" ";
+		}
+		cout<<endl;
+	}
+
+	int left = 0;
+	int right = tmpMat.cols;
+	int up = 0;
+	int down = tmpMat.rows;
+	// 
+	int num = 0;
+	//int range = tmpMat.cols / 5;
+	for(int i = 0; i < tmpMat.rows; i++)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.cols; j++)
+		{
+			if((int)tmpMat.at<uchar>(i,j) >= black)
+			{
+				num++;
+			}
+		}
+		if(num>=0.65* tmpMat.cols)
+		{
+			up = i;
+			break;
+		}
+	}
+	for(int i = tmpMat.rows-1; i >= 0; i--)
+	{
+		num  = 0;
+		for(int j = 0; j < tmpMat.cols; j++)
+		{
+			if((int)tmpMat.at<uchar>(i,j) >= black)
+			{
+				num ++;
+			}
+		}
+		if(num >=0.85* tmpMat.cols)
+		{
+			down = i;
+			break;
+		}
+	}
+	//range = tmpMat.rows / 5;
+	for(int i = 0; i < tmpMat.cols; i++)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.rows; j++)
+		{
+			if((int)tmpMat.at<uchar>(j,i) >= black)
+			{
+				num++;
+			}
+		}
+		if(num >=0.8* tmpMat.rows)
+		{
+			left = i;
+			break;
+		}
+	}
+	for(int i = tmpMat.cols -1; i >= 0; i--)
+	{
+		num = 0;
+		for(int j = 0; j < tmpMat.rows; j++)
+		{
+			if((int)tmpMat.at<uchar>(j,i) >= black)
+			{
+				num++;
+			}
+		}
+		if(num >=0.8* tmpMat.rows)
+		{
+			right = i;
+			break;
+		}
+	}
+	cout<<up<<" "<<down<<" "<<left<<" "<<right<<endl;
+	if(up > down || right < left)
+	{
+		cerr<<"image is total black:\t"<<imageName;
+		return 3;
+	}
+	Rect rect(left, up, right - left, down - up);
+	Mat imageSave = inputImg(rect);
+	imwrite(saveImageName, imageSave);
+	return 0;
+}
+
+// 
+
+void usage()
+{
+	cout<<"*******************************************"<<endl;
+	cout<<"./removeblack inputImage outPutImage"<<endl;
+}
+
+
+
 int main(int argc, char* argv[])
 {
 	//导入拼接图像  
@@ -27,11 +193,24 @@ int main(int argc, char* argv[])
 	namedWindow("全景拼接", 0);
 	imshow("全景拼接", pano);
 	imwrite("image_dest.jpg", pano);
+	//Mat a=pano.clone();
 	waitKey();
+	/*if(argc != 3)
+	{
+		cerr<<"parameter error";
+		usage();
+		return 1;
+	}*/
+	//string inputFileName = (string)(argv[1]);
+	//string outputFileName = (string)(argv[2]);
+	string inputFileName = ("image_dest.jpg");
+	string outputFileName = ("result.jpg");
+	imageRemove  iR(inputFileName, outputFileName);
+	iR.removeblack();
+
 	return 0;
 }
- 
-//导入所有原始拼接图像函数  
+
 void parseCmdArgs(int argc, char** argv)
 {
 	for (int i = 1; i < argc; i++)
@@ -43,4 +222,5 @@ void parseCmdArgs(int argc, char** argv)
 		}
 		imgs.push_back(img);
 	}
+
 }
